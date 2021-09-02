43b8b535a56f5f4e405bf0d51ac820a1815e09a7
richardhakkers <78424013+richardhakkers@users.noreply.github.com>
2021-09-02 13:54:26 +0200

    version 1

---
+++ helloworld.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "hello World\n";
+echo "hello World\n";
+echo "hello World\n";
+echo "hello World\n";
+echo "hello World\n";
+
+?>
---
+++ rekensommen.php
@@ -0,0 +1,24 @@
+<?php
+    $getal1=43;
+    $getal2=6;
+
+    $som=$getal1 + $getal2;
+    echo "$getal1 + $getal2 = " .$som;
+    echo "\\n";
+
+    $verschil=$getal1 - $getal2;
+    echo "$getal1 - $getal2 = " .$verschil;
+    echo "\\n";
+
+    $product=$getal1 * $getal2;
+    echo "$getal1 * $getal2 = " .$product;
+    echo "\\n";
+
+    $quotient=$getal1 / $getal2;
+    echo"$getal1 / $getal2 = " .$quotient;
+    echo "\\n";
+
+    $rest=$getal1 % $getal2;
+    echo "$getal1 % $getal2 = " .$rest;
+    echo "\\n";
+?>
